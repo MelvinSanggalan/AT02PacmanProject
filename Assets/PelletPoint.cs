074a15e5bc7e5d1856d7027cc221f8dd55b8eb0d
MelvinSanggalan <109941231+MelvinSanggalan@users.noreply.github.com>
2022-08-18 21:03:29 +0800

    Added Regular Pellet

---
+++ Assets/PelletPoint.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PelletPoint : MonoBehaviour
+{
+    public Text textRef;
+    public int scoreValue = 5;
+
+    //The value of a static variable is shared between all instance
+    //of a class. In this case, all instances oif the ScoreTrigger script
+    //will share the same value for this variable.
+    private static int currentScore = 0;
+
+    private void Awake()
+    {
+        if (currentScore > 0)
+        {
+            currentScore = 0;
+        }
+        textRef.text = "Score : " + currentScore;
+
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called on the frame when a foreign collider enters this collider.
+    /// A reference to the foreign collider is stored within the 'other' parameter.
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            currentScore += scoreValue;
+            textRef.text = "Score : " + currentScore;
+            gameObject.SetActive(false);
+
+        }
+    }
+
+}
